69d33d5ed8ffd3d69ada947f73795e7ed97ba609
Xushuchi <njuxushuchi@163.com>
2020-07-05 00:13:52 +0800

    update

---
+++ mooc/pipeline/cmd/externalsort/sort.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"demo/pipeline"
+	"fmt"
+	"os"
+)
+
+func main() {
+	p := createPipeline("small.in", 512, 4)
+	writeToFile(p, "small.out")
+	printFile("small.out")
+}
+
+func printFile(fileName string) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	p := pipeline.ReaderSource(file, -1)
+	for v := range p {
+		fmt.Println(v)
+	}
+}
+
+func writeToFile(p <-chan int, fileName string) {
+	file, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	pipeline.WriterSink(writer, p)
+}
+
+func createPipeline(filename string, fileSize, chunkCount int) <-chan int {
+	chunkSize := fileSize / chunkCount
+	var sortResults []<-chan int
+	for i := 0; i < chunkCount; i++ {
+		file, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+
+		file.Seek(int64(i*chunkSize), 0)
+
+		source := pipeline.ReaderSource(bufio.NewReader(file), chunkSize)
+		sortResults = append(sortResults, pipeline.InMemSort(source))
+	}
+
+	return MergeN(sortResults...)
+}
+
+func MergeN(inputs ...<-chan int) <-chan int {
+	if len(inputs) == 1 {
+		return inputs[0]
+	}
+	m := len(inputs) / 2
+
+	return pipeline.Merge(MergeN(inputs[:m]...), MergeN(inputs[m:]...))
+}
---
+++ mooc/pipeline/pipeline/nodes.go
@@ -0,0 +1,88 @@
+package pipeline
+
+import (
+	"encoding/binary"
+	"io"
+	"sort"
+)
+
+func ArraySource(a ...int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		for _, v := range a {
+			out <- v
+		}
+		close(out)
+	}()
+	return out
+}
+
+func InMemSort(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		var a []int
+		for v := range in {
+			a = append(a, v)
+		}
+
+		sort.Ints(a)
+
+		for _, v := range a {
+			out <- v
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func Merge(in1, in2 <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		v1, ok1 := <-in1
+		v2, ok2 := <-in2
+		for ok1 || ok2 {
+			if !ok2 || (ok1 && v1 <= v2) {
+				out <- v1
+				v1, ok1 = <-in1
+			} else {
+				out <- v2
+				v2, ok2 = <-in2
+			}
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func ReaderSource(reader io.Reader, chunkSize int) <-chan int {
+	out := make(chan int)
+	go func() {
+		buffer := make([]byte, 8)
+		bytesRead := 0
+		for {
+			n, err := reader.Read(buffer)
+			bytesRead += n
+			if n > 0 {
+				v := int(binary.BigEndian.Uint64(buffer))
+				out <- v
+			}
+			if err != nil || (chunkSize != -1 && bytesRead >= chunkSize) {
+				break
+			}
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func WriterSink(writer io.Writer, in <-chan int) {
+	for v := range in {
+		buffer := make([]byte, 8)
+		binary.BigEndian.PutUint64(buffer, uint64(v))
+		writer.Write(buffer)
+	}
+}
